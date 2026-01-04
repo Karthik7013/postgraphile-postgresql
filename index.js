631e1f9694213f34f064fa4408d9fc7ad21d44f2
AlienCoder <113322353+Karthik7013@users.noreply.github.com>
2026-01-04 23:01:53 +0530

    Disable persisted queries in Apollo Server
    Added a configuration to disable persisted queries in Apollo Server to enhance security.

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 import express from 'express';
 import { createPostGraphQLSchema } from 'postgraphile';
 import fs from 'fs';
@@ -21,6 +22,7 @@ async function startServer() {
 
     const server = new ApolloServer({
         schema,
+        persistedQueries: false,  // Added: Disable persisted queries to prevent DoS vulnerability
         context: async () => {
             const pgClient = await pgPool.connect();
             await pgClient.query('BEGIN');
